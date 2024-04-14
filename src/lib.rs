165e6e5bf1aa753b6064ff6567524189da9a71bd
clankpan <clankpan26cm@gmail.com>
2024-04-14 12:38:21 -0700

    move PointInterface trait to another file

--- src/lib.rs
+++ src/lib.rs
@@ -20,73 +20,10 @@ use indicatif::ProgressBar;
 #[cfg(feature = "indicatif")]
 use std::sync::atomic::{self, AtomicUsize};
 
-/// Traits that the Point type should implement for use by vectune::Builder.
-///
-/// # Examples
-///
-/// ```rust
-/// #[derive(Serialize, Deserialize, Clone, Debug)]
-/// struct Point(Vec<f32>);
-/// impl PointInterface for Point {
-///     ...
-/// }
-/// ```
-pub trait PointInterface: Clone + Sync {
-    /// A function that returns the distance between two Points. Typically, the Euclidean distance is used.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// fn distance(&self, other: &Self) -> f32 {
-    ///     self.0
-    ///         .iter()
-    ///         .zip(other.0.iter())
-    ///         .map(|(a, b)| {
-    ///             let c = a - b;
-    ///             c * c
-    ///         })
-    ///         .sum::<f32>()
-    ///         .sqrt()
-    /// }
-    /// ```
-    fn distance(&self, other: &Self) -> f32;
+pub mod traits;
 
-    /// The number of dimensions of a vector.
-    fn dim() -> u32;
+pub use crate::traits::point::PointInterface;
 
-    /// Addition of two Points. Used by Builder to find the centroid.
-    ///
-    ///　# Examples
-    ///
-    /// ```rust
-    /// fn add(&self, other: &Self) -> Self {
-    ///     Point::from_f32_vec(
-    ///         self.to_f32_vec()
-    ///             .into_iter()
-    ///             .zip(other.to_f32_vec().into_iter())
-    ///             .map(|(x, y)| x + y)
-    ///             .collect(),
-    ///     )
-    /// }
-    /// ```
-    fn add(&self, other: &Self) -> Self;
-
-    /// Division of a Point. Used by Builder to find the centroid.
-    ///
-    ///　# Examples
-    ///
-    /// ```rust
-    /// fn div(&self, divisor: &usize) -> Self {
-    ///     Point::from_f32_vec(
-    ///         self.to_f32_vec()
-    ///             .into_iter()
-    ///             .map(|v| v / *divisor as f32)
-    ///             .collect(),
-    ///     )
-    /// }
-    /// ```
-    fn div(&self, divisor: &usize) -> Self;
-}
 
 /// Builder is a structure and implementation for creating a Vamana graph.
 ///
---
+++ src/traits/point.rs
@@ -0,0 +1,69 @@
+
+
+/// Traits that the Point type should implement for use by vectune::Builder.
+///
+/// # Examples
+///
+/// ```rust
+/// #[derive(Serialize, Deserialize, Clone, Debug)]
+/// struct Point(Vec<f32>);
+/// impl PointInterface for Point {
+///     ...
+/// }
+/// ```
+pub trait PointInterface: Clone + Sync {
+  /// A function that returns the distance between two Points. Typically, the Euclidean distance is used.
+  ///
+  /// # Examples
+  ///
+  /// ```rust
+  /// fn distance(&self, other: &Self) -> f32 {
+  ///     self.0
+  ///         .iter()
+  ///         .zip(other.0.iter())
+  ///         .map(|(a, b)| {
+  ///             let c = a - b;
+  ///             c * c
+  ///         })
+  ///         .sum::<f32>()
+  ///         .sqrt()
+  /// }
+  /// ```
+  fn distance(&self, other: &Self) -> f32;
+
+  /// The number of dimensions of a vector.
+  fn dim() -> u32;
+
+  /// Addition of two Points. Used by Builder to find the centroid.
+  ///
+  ///　# Examples
+  ///
+  /// ```rust
+  /// fn add(&self, other: &Self) -> Self {
+  ///     Point::from_f32_vec(
+  ///         self.to_f32_vec()
+  ///             .into_iter()
+  ///             .zip(other.to_f32_vec().into_iter())
+  ///             .map(|(x, y)| x + y)
+  ///             .collect(),
+  ///     )
+  /// }
+  /// ```
+  fn add(&self, other: &Self) -> Self;
+
+  /// Division of a Point. Used by Builder to find the centroid.
+  ///
+  ///　# Examples
+  ///
+  /// ```rust
+  /// fn div(&self, divisor: &usize) -> Self {
+  ///     Point::from_f32_vec(
+  ///         self.to_f32_vec()
+  ///             .into_iter()
+  ///             .map(|v| v / *divisor as f32)
+  ///             .collect(),
+  ///     )
+  /// }
+  /// ```
+  fn div(&self, divisor: &usize) -> Self;
+}
